b12a4c563823ee4400e7ac6d7013746ae39d10e6
Aaron Saloff <aaronsaloff@gmail.com>
2017-11-01 06:39:34 -0400

    added production database URL for seeder

--- mongo.js
+++ mongo.js
@@ -7,9 +7,5 @@ module.exports = () => {
   var localUrl = `mongodb://${ config.host }/${ config.database }`;
   var mongoUrl =  envUrl ? envUrl : localUrl;
 
-  console.log(`config: ${config}`)
-  console.log(`process.env[config.use_env_variable]: ${ envUrl }`);
-  console.log(`connecting to Mongo: ${ mongoUrl }`);
-
   return mongoose.connect(mongoUrl);
 };
--- seeds/index.js
+++ seeds/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const mongooseeder = require('mongooseeder');
 const models = require('./../models');
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/mongo')[env];
 
-const mongodbUrl = 'mongodb://localhost/demo_exploring_mongoose_development';
+const envUrl = process.env[config.use_env_constiable];
+const localUrl = `mongodb://${ config.host }/${ config.database }`;
+const mongodbUrl =  envUrl ? envUrl : localUrl;
 
 mongooseeder.seed({
   mongodbUrl: mongodbUrl,
